a8712ea76e799e788e1ac00217fadc6ead09303e
laksh <laksh@DESKTOP-ACPQAES.mshome.net>
2024-11-22 12:08:09 +0530

    Example 2

---
+++ firstMavanProject/src/main/java/firstMavanPackage/MavenEx2.java
@@ -0,0 +1,17 @@
+package firstMavanPackage;
+
+public class MavenEx2 {
+	public void printvalue()
+	{
+		String n="NAME";
+		System.out.println(n);
+	}
+
+	public static void main(String[] args) {
+		MavenEx2 obj=new MavenEx2();
+		obj.printvalue();
+		// TODO Auto-generated method stub
+
+	}
+
+}
