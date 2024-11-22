b7c2075cbed7ae34efa401f4d6244732eb58540f
laksh <laksh@DESKTOP-ACPQAES.mshome.net>
2024-11-22 11:59:51 +0530

    My First Commit

---
+++ firstMavanProject/src/main/java/firstMavanPackage/MavanExample1.java
@@ -0,0 +1,19 @@
+package firstMavanPackage;
+
+public class MavanExample1 {
+	public void add()
+	{ int a=10;
+		int b= 20;
+		int c= a+b;
+		System.out.println("Result: "+c);
+	}
+
+	public static void main(String[] args) {
+		MavanExample1 obj=new MavanExample1();
+		obj.add();
+			// TODO Auto-generated method stub
+		}
+
+	}
+
+
